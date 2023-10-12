33ec524c8f605f88622c2604b9d5bac9386df1d4
Erica de Bem <ericadebemm@gmail.com>
2023-10-12 19:04:26 +0200

    test1

---
+++ main.js
@@ -0,0 +1,27 @@
+/* Variaveis 
+let nome = "Erica" (esta correto)
+nome = 10 (correto tbm)
+O resultado esperado sera 10.
+
+----
+*/
+let nome = "Erica";
+
+    if (true) {
+        let nome = "Oi";
+        console.log(nome);
+}
+
+console.log(nome);
+
+
+
+
+
+
+
+
+
+
+
+
